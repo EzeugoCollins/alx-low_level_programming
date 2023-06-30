8568a6b275a5118145d0875e77e5b6541e0c53b6
EzeugoCollins <ezeugocollins4@gmail.com>
2023-06-29 21:37:31 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings values
+ * @s1: input value
+ * @s2: input value
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int counter, compare_value;
+
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+	{
+		counter++;
+	}
+	compare_value = s1[counter] - s2[counter];
+	return (counter_value);
+}
